f888b411d27a05eb22a634431925ed198016a831
rstap1 <115902899+rstap1@users.noreply.github.com>
2025-12-05 16:27:25 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// SERVE PORTFOLIO
+app.use(express.static(path.join(__dirname, "public")));
+
+// MOUNT API ROUTE
+const movieRoutes = require("./routes/movie-Routes");
+app.use("/api", movieRoutes);
+
+// START SERVER
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
